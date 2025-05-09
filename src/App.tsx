3e511a505c1ac13b1bce830657ad857685c5cabb
Abdullah <abdulhhany69@gmail.com>
2025-05-09 14:56:55 +0300

    Enhance add product modal form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import ProductCard from "./conponents/ProductCard";
 import Button from "./conponents/ui/Button";
+import Input from "./conponents/ui/Input";
 import Modal from "./conponents/ui/Modal";
-import { productList } from "./data";
+import { formInputsList, productList } from "./data";
 import { useState } from "react";
 
 const App = () => {
@@ -21,18 +22,34 @@ const App = () => {
   const renderProductList = productList.map((product) => (
     <ProductCard key={product.id} product={product} />
   ));
+  const renderFormInputList = formInputsList.map((input) => (
+    <div className="flex flex-col">
+      <label
+        htmlFor={input.id}
+        className="text-gray-700 text-sm font-medium mb-[2px]"
+      >
+        {input.label}
+      </label>
+      <Input type="text" id={input.id} name={input.name} />
+    </div>
+  ));
   return (
     <main className="container">
-          <Button className="bg-blue-700 hover:bg-blue-800" onClick={openModal}>Add</Button>
+      <Button className="bg-blue-700 hover:bg-blue-800" onClick={openModal}>
+        Add
+      </Button>
 
       <div className="grid grid-cols-1 p-2 m-5 rounded-md mdgap-2 md:grid-cols-3 sm:grid-cols-2 xl:grid-cols-5">
         {renderProductList}
       </div>
       <Modal isOpen={isOpen} closeModal={closeModal} title="ADD A NEW PRODUCT">
-        <div className="flex items-center space-x-3">
-          <Button className="bg-blue-700 hover:bg-blue-800">Cancel</Button>
-          <Button className="bg-gray-300 hover:bg-gray-800">Submit</Button>
-        </div>
+        <form className="space-y-3">
+          {renderFormInputList}
+          <div className="flex items-center space-x-3">
+            <Button className="bg-blue-700 hover:bg-blue-800">Cancel</Button>
+            <Button className="bg-gray-300 hover:bg-gray-500">Submit</Button>
+          </div>
+        </form>
       </Modal>
     </main>
   );
--- src/conponents/ui/Button.tsx
+++ src/conponents/ui/Button.tsx
@@ -3,15 +3,14 @@ import type { ButtonHTMLAttributes, ReactNode } from "react";
 interface IProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   className?: string;
-  width?: 'w-full' | 'w-fit';
+  width?: "w-full" | "w-fit";
 }
 
-const Button = ({ children, className, width = 'w-full', ...rest }: IProps) => {
+const Button = ({ children, className, width = "w-full", ...rest }: IProps) => {
   return (
     <button
-      className={`${className} ${width} p-2   rounded-md text-white `}
+      className={`${className} ${width} p-2 rounded-md text-white `}
       {...rest}
-      
     >
       {children}
     </button>
